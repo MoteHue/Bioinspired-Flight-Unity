ad85a02bfebed517294bbe40bffd62b69944e6c1
Unknown <matthew.crees@ntlworld.com>
2020-06-07 19:02:21 +0100

    Iffy force-based movement

--- Bioinspired Flight/Assets/Scripts/PlayerController.cs
+++ Bioinspired Flight/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
@@ -10,21 +11,37 @@ public class PlayerController : MonoBehaviour
     public Slider heightSlider;
     public Slider rotationSlider;
 
-    public float moveSpeed = 0.1f;
-    public float ascentSpeed = 0.1f;
-    public float rotationSpeed = 0.1f;
+    public float gravity = 0.1f;
+    public float horizontalAcceleration = 2f;
+    public float maxMoveSpeed = 10f;
+    public float rotationSpeed = 0.5f;
 
-    Vector3 velocity = new Vector3();
+    public float verticalAcceleration = 5f;
+    public float maxAscentSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        velocity.x = joystick.Horizontal * moveSpeed;
-        velocity.z = joystick.Vertical  * moveSpeed;
-        velocity.y = heightSlider.value * ascentSpeed;
+    private void Start() {
 
-        transform.position += velocity;
-        transform.Rotate(0f, rotationSlider.value * rotationSpeed, 0f, Space.Self);
+    }
+
+    private void Update() {
+        float x = joystick.Horizontal * horizontalAcceleration;
+        float z = joystick.Vertical * horizontalAcceleration;
+        float y = heightSlider.value * verticalAcceleration;
+        float rot = rotationSlider.value;
+        Quaternion rotation = Quaternion.Euler(0f, rot, 0f);
+
+        rb.AddRelativeForce(new Vector3(x, y, z));
+
+        rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -maxAscentSpeed, maxAscentSpeed),rb.velocity.z);
+        rb.MoveRotation(rotation);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        
     }
 
 }
