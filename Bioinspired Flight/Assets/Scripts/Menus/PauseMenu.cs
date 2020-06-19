4c22bf3561f2127f8086effe6aaa0db8a8e6731a
Unknown <matthew.crees@ntlworld.com>
2020-06-19 14:37:28 +0100

    Simple pause menu

---
+++ Bioinspired Flight/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public void ResumeGame() {
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void ExitGame() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PauseGame() {
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+}
