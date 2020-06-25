994a071e76557e600ef0d64ad95f42408e9d9055
Skyrah1 <zhunchenyek1@gmail.com>
2020-06-25 14:58:31 +0100

    Application can now save data

--- Bioinspired Flight/Assets/Scripts/Menus/MainMenu.cs
+++ Bioinspired Flight/Assets/Scripts/Menus/MainMenu.cs
@@ -5,8 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private SaveData achievementData;
+
+    void Awake(){
+        achievementData = new SaveData("Achievements.save");
+        achievementData.Load();
+    }
 
     public void PlayGame() {
+        bool firstFlight = false;
+        if (achievementData.data.ContainsKey("First Flight")){
+            firstFlight = achievementData.data["First Flight"];
+        }
+        if (!firstFlight){
+            achievementData.data["First Flight"] = true;
+            achievementData.Save();
+            UnityEngine.Debug.Log("First time?");
+        } else {
+            UnityEngine.Debug.Log("You've done this before, you'll be fine.");
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
