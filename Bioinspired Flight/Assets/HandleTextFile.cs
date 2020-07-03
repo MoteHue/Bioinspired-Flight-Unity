f980c5f35eb094baa3d7f20a6d96642ddd45f420
eb18608 <eb18608@bristol.ac.uk>
2020-07-03 14:34:01 +0100

    Testing the waters with customizations array
    loadoutArray to created with values reflecting Loadout.save values... Thanks Yek for the save file system.

---
+++ Bioinspired Flight/Assets/HandleTextFile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class HandleTextFile : MonoBehaviour
+{
+
+    [MenuItem("Tools/Read file")]
+    public string ReadString()
+    {
+        string path = "Assets/Sensor Information/test.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string text = reader.ReadToEnd();
+        reader.Close();
+        Debug.Log(text);
+        return text;
+    }
+
+}
--- Bioinspired Flight/Assets/Scripts/Menus/MainMenu.cs
+++ Bioinspired Flight/Assets/Scripts/Menus/MainMenu.cs
@@ -1,32 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     private SaveData achievementData;
+    private SaveData loadoutData;
 
     void Awake(){
         achievementData = new SaveData("Achievements.save");
         achievementData.Load();
+        loadoutData = new SaveData("Loadout.save");
+        loadoutData.Load();
     }
 
     public void PlayGame() {
         bool firstFlight = false;
-        if (achievementData.data.ContainsKey("First Flight")){
+        if (achievementData.data.ContainsKey("First Flight")) {
             firstFlight = achievementData.data["First Flight"];
         }
         if (!firstFlight){
+            //Setup First time achievement data
             achievementData.data["First Flight"] = true;
             achievementData.Save();
+
+
             UnityEngine.Debug.Log("First time?");
         } else {
             UnityEngine.Debug.Log("You've done this before, you'll be fine.");
+
         }
         SceneManager.LoadScene( 2 );
     }
 
+    // When Customizations is pressed:
+    // First Time: Create Loadout.save to save loadout preferences
+    // Any other time: Reassign loadoutArray to reflect values in
+    // previously saved Loadout.save
+    public void Customizations()
+    {
+        bool firstLoadout = false;
+        bool[] loadoutArray = new bool[2];
+        if (loadoutData.data.ContainsKey("Loadout")){
+            firstLoadout = loadoutData.data["Loadout"];
+            loadoutArray[0] = loadoutData.data["Feathers"];
+            loadoutArray[1] = loadoutData.data["Turtle"];
+            UnityEngine.Debug.Log("Made your last saved loadout");
+        }
+
+        // First loadout
+        if (!firstLoadout)
+        {
+            // Set Loadout value to true
+            loadoutData.data["Loadout"] = true;
+            // Default loadout
+            loadoutData.data["Feathers"] = false;
+            loadoutData.data["Turtle"] = true;
+
+            // Save intitial loadout
+            loadoutData.Save();
+
+            UnityEngine.Debug.Log("Loadouts created at lvl4");
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Loadout already there");
+            UnityEngine.Debug.Log(loadoutArray[0]);
+            UnityEngine.Debug.Log(loadoutArray[1]);
+        }
+    }
+
+    // Used to test whether you can freely change loadout values
+    // whilst having it update in customizations
+    public void testChangeLoadout()
+    {
+        UnityEngine.Debug.Log("Fine I'll give you feathers");
+        loadoutData.data["Feathers"] = true;
+        if (loadoutData.data["Feathers"])
+        {
+            UnityEngine.Debug.Log("So it changes even without saving but it worked");
+        }
+
+        loadoutData.Save();
+
+    }
     public void QuitGame() {
         Debug.Log("Quit!");
         Application.Quit();
