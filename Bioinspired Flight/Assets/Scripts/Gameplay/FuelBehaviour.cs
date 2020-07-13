b5b31686dd2737aeecad3c827c9f6d6141beca5c
Unknown <matthew.crees@ntlworld.com>
2020-07-13 13:43:24 +0100

    ground texture fixed with shadows, fuel pickups added

---
+++ Bioinspired Flight/Assets/Scripts/Gameplay/FuelBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
+    }
+}
