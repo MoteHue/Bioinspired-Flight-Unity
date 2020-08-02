54051b550c09e64052b26b39ab183a75c8001dfb
eb18608 <eb18608@bristol.ac.uk>
2020-08-02 16:40:34 +0100

    Setting up Menu
    Options for Audio slider and quality adjustment

---
+++ Bioinspired Flight/Assets/SettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+    
+
+    public void setVolume(float i)
+    {
+        mainMixer.SetFloat("mainVolume" , i);
+    }
+
+    public void setGraphics(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+
+}
